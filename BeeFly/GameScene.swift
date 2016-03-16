71ea53180c45519321ff6303e9e610ec9453be64
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2016-03-16 09:01:17 +0700

    [FINISH] Menu

--- BeeFly/GameScene.swift
+++ BeeFly/GameScene.swift
@@ -12,28 +12,69 @@ struct PhysicsCategory {
     static let land: UInt32 = 0x1 << 1
     static let tree: UInt32 = 0x1 << 2
     static let bird: UInt32 = 0x1 << 3
+    static let score: UInt32 = 0x1 << 4
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var bird = SKSpriteNode()
     var node = SKNode()
     var moveAndRemove = SKAction()
     var gameStarted = Bool()
+    var score: Int = 0
+    var lblScore = SKLabelNode()
+    var died = Bool()
+    
+    var btnReload = SKSpriteNode()
+    var btnMenu = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        let scoreNode = SKSpriteNode(imageNamed: "score.png")
-        scoreNode.setScale(0.5)
-        scoreNode.position = CGPointMake(self.size.width / 3 + 20, self.size.height - 40)
-        scoreNode.zPosition = 3
+        self.createStartGameScene()
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let bodyContactA = contact.bodyA
+        let bodyContactB = contact.bodyB
         
-        let lbl = SKLabelNode()
-        lbl.text = String(100)
-        scoreNode.addChild(lbl)
+        if bodyContactA.categoryBitMask == PhysicsCategory.tree && bodyContactB.categoryBitMask == PhysicsCategory.bird || bodyContactB.categoryBitMask == PhysicsCategory.tree && bodyContactA.categoryBitMask == PhysicsCategory.bird || bodyContactA.categoryBitMask == PhysicsCategory.land && bodyContactB.categoryBitMask == PhysicsCategory.bird || bodyContactB.categoryBitMask == PhysicsCategory.land && bodyContactA.categoryBitMask == PhysicsCategory.bird {
+            died = true
+            
+            self.removeAllActions()
+            node.removeAllActions()
+            bird.removeAllActions()
+            
+            self.createRestartGameScene()
+            
+            let birdAct3 = SKTexture(imageNamed: "bird_blue_4")
+            let changeAct = SKAction.animateWithTextures([birdAct3], timePerFrame: NSTimeInterval(0.2))
+            bird.runAction(changeAct)
+        }
         
-        self.addChild(scoreNode)
+        if bodyContactA.categoryBitMask == PhysicsCategory.score && bodyContactB.categoryBitMask == PhysicsCategory.bird || bodyContactB.categoryBitMask == PhysicsCategory.score && bodyContactA.categoryBitMask == PhysicsCategory.bird {
+            score++
+            lblScore.text = String(score)
+        }
+    }
+    
+    func createStartGameScene() {
+        self.physicsWorld.contactDelegate = self
+        
+        let src = SKSpriteNode(imageNamed: "score.png")
+        src.position = CGPointMake(self.size.width / 3 + 20, self.size.height - 40)
+        src.zPosition = 4
+        src.setScale(0.5)
+        
+        lblScore = SKLabelNode(fontNamed: "debussy")
+        lblScore.text = String(0)
+        lblScore.zPosition = 4
+        lblScore.fontSize = 40
+        lblScore.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
+        lblScore.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        src.addChild(lblScore)
+        
+        self.addChild(src)
         
         let bg = SKSpriteNode(imageNamed: "background.png")
         bg.position = CGPointMake(self.size.width / 2, self.size.height / 2)
@@ -57,14 +98,14 @@ class GameScene: SKScene {
         
         /* Bird */
         bird = SKSpriteNode(imageNamed: "bird_blue_1.png")
-        bird.setScale(0.18)
+        bird.setScale(0.15)
         bird.zPosition = 1
         bird.position = CGPointMake(self.frame.width / 2, self.size.height / 2)
         
         bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.frame.height / 2)
         bird.physicsBody?.categoryBitMask = PhysicsCategory.bird
         bird.physicsBody?.collisionBitMask = PhysicsCategory.land | PhysicsCategory.tree
-        bird.physicsBody?.contactTestBitMask = PhysicsCategory.land | PhysicsCategory.tree
+        bird.physicsBody?.contactTestBitMask = PhysicsCategory.land | PhysicsCategory.tree | PhysicsCategory.score
         bird.physicsBody?.affectedByGravity = true
         bird.physicsBody?.dynamic = false
         
@@ -79,12 +120,45 @@ class GameScene: SKScene {
         self.addChild(bird)
     }
     
+    func createRestartGameScene() {
+        let restartGame = SKNode()
+        restartGame.zPosition = 6
+        
+        let bg = SKSpriteNode(imageNamed: "info-background.png")
+        bg.position = CGPointMake(self.frame.width / 2, self.frame.height / 2 + 50)
+        
+        btnReload = SKSpriteNode(imageNamed: "btn-reload")
+        btnReload.position = CGPointMake(self.frame.width / 2 - 60, self.frame.height / 2 + 30)
+        btnReload.zPosition = 7
+        self.addChild(btnReload)
+        
+        btnMenu = SKSpriteNode(imageNamed: "btn-menu")
+        btnMenu.position = CGPointMake(self.frame.width / 2 + 60, self.frame.height / 2 + 30)
+        btnMenu.zPosition = 7
+        self.addChild(btnMenu)
+        
+        restartGame.addChild(bg)
+        self.addChild(restartGame)
+    }
+    
     func createTrees() {
         node = SKNode()
         
+        let scoreNode = SKSpriteNode()
+        scoreNode.size = CGSize(width: 1, height: 400)
+        scoreNode.position = CGPointMake(self.frame.width, self.frame.height / 2)
+        
+        scoreNode.physicsBody = SKPhysicsBody(rectangleOfSize: scoreNode.size)
+        scoreNode.physicsBody?.categoryBitMask = PhysicsCategory.score
+        scoreNode.physicsBody?.collisionBitMask = 0
+        scoreNode.physicsBody?.contactTestBitMask = PhysicsCategory.bird
+        scoreNode.physicsBody?.affectedByGravity = false
+        scoreNode.physicsBody?.dynamic = false
+
+        
         let treeTop = SKSpriteNode(imageNamed: "tree.png")
         treeTop.setScale(0.5)
-        treeTop.position = CGPointMake(self.size.width, self.size.height / 2 + 280)
+        treeTop.position = CGPointMake(self.size.width, self.size.height / 2 + 265)
         treeTop.zRotation = CGFloat(M_PI)
         treeTop.zPosition = 1
         
@@ -99,7 +173,7 @@ class GameScene: SKScene {
         
         let treeBottom = SKSpriteNode(imageNamed: "tree.png")
         treeBottom.setScale(0.5)
-        treeBottom.position = CGPointMake(self.size.width, self.size.height / 2 - 180)
+        treeBottom.position = CGPointMake(self.size.width, self.size.height / 2 - 185)
         treeBottom.zPosition = 1
         
         treeBottom.physicsBody = SKPhysicsBody(rectangleOfSize: treeBottom.frame.size)
@@ -114,14 +188,23 @@ class GameScene: SKScene {
         let randomPos = CGFloat.random(min: -50, max: 50)
         node.position.y = node.position.y + randomPos
         node.runAction(moveAndRemove)
+        node.addChild(scoreNode)
         
         self.addChild(node)
+    }
+    
+    func actRestartGame() {
+        gameStarted = false
+        score = 0
+        died = false
+        self.removeAllChildren()
+        self.createStartGameScene()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if gameStarted == false {
             gameStarted = true
-            let moveBackBird = SKAction.moveToX(self.frame.width / 2 - bird.frame.width, duration: 1)
+            let moveBackBird = SKAction.moveToX(self.frame.width / 2 - bird.frame.width, duration: 4)
             bird.runAction(moveBackBird)
             bird.physicsBody?.dynamic = true
             
@@ -142,10 +225,19 @@ class GameScene: SKScene {
             moveAndRemove = SKAction.sequence([moveTrees, removeTrees])
             
             bird.physicsBody?.velocity = CGVectorMake(0, 0)
-            bird.physicsBody?.applyImpulse(CGVectorMake(0, 120))
+            bird.physicsBody?.applyImpulse(CGVectorMake(0, 70))
         } else {
-            bird.physicsBody?.velocity = CGVectorMake(0, 0)
-            bird.physicsBody?.applyImpulse(CGVectorMake(0, 120))
+            if !died {
+                bird.physicsBody?.velocity = CGVectorMake(0, 0)
+                bird.physicsBody?.applyImpulse(CGVectorMake(0, 70))
+            } else {
+                for touch in touches {
+                    let location = touch.locationInNode(self)
+                    if btnReload.containsPoint(location) {
+                        self.actRestartGame()
+                    }
+                }
+            }
         }
     }
    
